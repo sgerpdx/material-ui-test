dd6c1e94fa917cb69991b4dea140e5f0132d81ec
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-05 09:57:43 -0700

    added input components + font + favicon

--- src/components/MaterialComponents.jsx
+++ src/components/MaterialComponents.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import Slider from '@material-ui/core/Slider';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default function MaterialComponents() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Box color="red">
+        <Container maxWidth="sm">
+          <Button variant="contained">Default</Button>
+          <Checkbox />
+          <InputLabel id="label">Age</InputLabel>
+          <Select labelId="label" id="select" value="20">
+            <MenuItem value="10">Ten</MenuItem>
+            <MenuItem value="20">Twenty</MenuItem>
+          </Select>
+          <Slider
+            defaultValue={0.00000005}
+            aria-labelledby="discrete-slider-small-steps"
+            step={0.00000001}
+            marks
+            min={-0.00000005}
+            max={0.0000001}
+            valueLabelDisplay="auto"
+          />
+        </Container>
+      </Box>
+    </div>
+  );
 }
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import MaterialComponents from '../MaterialComponents';
 import Button from '@material-ui/core/Button';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['ZCOOL KuaiLe', 'cursive'].join(','),
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Button variant="contained" color="primary">
         Hello World
       </Button>
       <MaterialComponents />
-    </>
+    </ThemeProvider>
   );
 }
