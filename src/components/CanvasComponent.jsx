e0bcc426fa4eca147a8d7b201db4a6ce234443fc
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-05 11:36:49 -0700

    text added to canvas

--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -3,14 +3,23 @@ import React, { useRef, useEffect } from 'react';
 export default function CanvasComponent(props) {
   const canvasRef = useRef(null);
 
+  const draw = (ctx) => {
+    ctx.beginPath();
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = '#CC00FF';
+    ctx.lineStyle = '#ffff00';
+    ctx.font = '18px sans-serif';
+    ctx.fillText('hey', 50, 50);
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-    //Our first draw
-    context.fillStyle = '#000000';
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-  }, []);
 
+    //Our draw come here
+    draw(context);
+  }, [draw]);
   return (
     <>
       <canvas ref={canvasRef} {...props} />
